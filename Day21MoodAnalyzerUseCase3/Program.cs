1a6d0d9e8a5ffc7e0d94dce646089ac638144665
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-12 15:27:38 +0530

    ok

---
+++ Day21MoodAnalyzerUseCase3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day21MoodAnalyzerUseCase3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase3 obj = new UseCase3();
+            string msg = obj.analyzeMood("My mood is ok ok type");
+            Console.WriteLine(msg);
+        }
+    }
+}
---
+++ Day21MoodAnalyzerUseCase3/UseCase3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day21MoodAnalyzerUseCase3
+{
+    public class UseCase3
+    {
+        int num3, num1 = 5, num2 = 0;
+        public string analyzeMood(string msg)
+        {
+            try
+            {
+                if (msg.Contains("happy"))
+                    return "Happy";
+                else
+
+                    num3 = num1 / num2;
+            }catch(Exception e)
+            {
+                Console.WriteLine("Invalid Mood : " + e.Message);
+            }
+            return "Please Enter Valid Mood";
+        }
+    }
+}
