b6ae574e493c9e5bf3f1cf5b2a255db19454505f
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-16 11:37:20 +0530

    ok

---
+++ Day22MoodAnalyzerUseCase1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day21MoodAnalyzerUseCase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase1 obj = new UseCase1();
+            string msg=obj.analyzeMood("My mood is happy");
+            Console.WriteLine(msg);
+        }
+    }
+}
