b797909dab08d2693c6e943fdbb34f9367ec826b
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-17 10:51:30 +0530

    ok

---
+++ Day21MoodAnalyzerUseCase3.1/MoodAnalysisException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day21MoodAnalyzerUseCasee3
+{
+    public class MoodAnalysisException:Exception
+    {
+        public MoodAnalysisException(string msg):base(msg)
+        {
+
+        }
+    }
+}
---
+++ Day21MoodAnalyzerUseCase3.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day21MoodAnalyzerUseCasee3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase1 obj = new UseCase1();
+            try
+            {
+                obj.analyzeMood("My mood is null");
+            }
+            catch (MoodAnalysisException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
