ad9048fcb2bc5eb611da0037d7b6c786005d5e6e
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-17 10:53:45 +0530

    ok

---
+++ Day21MoodAnalyzerUseCase6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+namespace Day21MoodAnalyzerUseCasee3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = typeof(Day21MoodAnalyzerUseCasee3.UseCase1);
+            MethodInfo[] mi = t.GetMethods();
+            foreach (MethodInfo method in mi)
+            {
+                Console.WriteLine(method);
+            }
+            UseCase1 obj = new UseCase1();
+            try
+            {
+                obj.analyzeMood("My mood is happy");
+            }
+            catch (MoodAnalysisException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
